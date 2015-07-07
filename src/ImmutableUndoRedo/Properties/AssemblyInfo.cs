7b766cf91a3da86318f4eac806c300fa66c6bc6b
Oliver Zick <oliver@oliverzick.de>
2015-07-07 17:02:37 +0200

    Remove comments.

--- src/ImmutableUndoRedo/Properties/AssemblyInfo.cs
+++ src/ImmutableUndoRedo/Properties/AssemblyInfo.cs
@@ -21,9 +21,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("ImmutableUndoRedo")]
 [assembly: AssemblyDescription("A lightweight, flexible and easy to use do/undo/redo implementation based on immutable objects for .NET.")]
 [assembly: AssemblyConfiguration("")]
@@ -33,23 +30,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("ef605850-4540-4e3a-9e71-cc10385d94c7")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
