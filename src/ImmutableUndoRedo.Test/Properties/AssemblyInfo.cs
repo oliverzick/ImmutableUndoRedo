695964de7a49cbb176a66801b285f8d96dfbbeb1
Oliver Zick <oliver@oliverzick.de>
2015-07-10 18:40:56 +0200

    Update assembly information and add file header.

--- src/ImmutableUndoRedo.Test/Properties/AssemblyInfo.cs
+++ src/ImmutableUndoRedo.Test/Properties/AssemblyInfo.cs
@@ -1,36 +1,38 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region Copyright and license
+// <copyright file="AssemblyInfo.cs" company="Oliver Zick">
+//     Copyright (c) 2015 Oliver Zick. All rights reserved.
+// </copyright>
+// <author>Oliver Zick</author>
+// <license>
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+// </license>
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über folgende 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("ImmutableUndoRedo.Test")]
+[assembly: AssemblyTitle("ImmutableUndoRedo Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImmutableUndoRedo.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("ImmutableUndoRedo Tests")]
+[assembly: AssemblyCopyright("Copyright © 2015 Oliver Zick. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
-// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("c6350e77-4af6-488a-bcd4-918ccf5f8d7f")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// durch Einsatz von '*', wie in nachfolgendem Beispiel:
-// [Assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
